3981b144e450f5d67408478a5b42814cdcf7eae7
rett gerst <rettgerst@users.noreply.github.com>
2021-04-04 15:40:46 -0400

    change text on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 
 import Button from 'components/Button';
+import questions from 'questions.json';
 
 import styles from './index.module.scss';
 
@@ -13,9 +14,9 @@ export default function Home() {
 				<Image height={1300} id="banner" src="/compass.png" width={1850} />
 			</div>
 			<Button href="/quiz/0">Click here to start!</Button>
-
-			<h2>What is SapplyValues?</h2>
-			<p>
+			<h2>What is NextCompass?</h2>
+			<p>NextCompass is based on SapplyValues, which describes itself as:</p>
+			<blockquote>
 				SapplyValues is a political compass test that combines the questions of
 				the <a href="http://sapplypoliticalcompass.com/">Sapply test</a>* with
 				the UI of <a href="https://8values.github.io/">8values</a>. You will be
@@ -26,14 +27,22 @@ export default function Home() {
 				questions have been reworded as I feel some of them were poorly written.
 				There are{' '}
 				<b>
-					<u>
-						<span id="numOfQuestions" />
-					</u>
+					<u>{questions.length}</u>
 				</b>{' '}
 				questions in the test.
+			</blockquote>
+			<p>
+				Additionally, NextCompass is a React/Next.js port of the SapplyValues
+				project. The functionality is the same as SapplyValues but may be
+				improved open in the future. It also works without JavaScript enabled!
+			</p>
+			<p>
+				The source code is available{' '}
+				<a href="https://github.com/rettgerst/nextcompass">on GitHub</a>.
 			</p>
 			<h2>Why use this version?</h2>
-			<p>
+			<p>Original text from SapplyValues:</p>
+			<blockquote>
 				Both the original and the popular clone version have a number of issues
 				and are very poorly coded. For starters, many users run into NaN errors.
 				This happens when using the keyboard to press enter to go to the next
@@ -47,30 +56,10 @@ export default function Home() {
 				with parameters in your history so you don&apos;t have to retake the
 				whole test. Finally, the 8values / 9Axes UI is far better (in my opinion
 				anyway). It looks nicer and takes less clicks to answer.
-			</p>
-			<h2>Your questions / results suck!</h2>
+			</blockquote>
 			<p>
-				You will have to complain to the original Sapply creator. I did not make
-				this test, I simply changed the UI.
-			</p>
-			<h2>Privacy notice</h2>
-			<p>
-				At the end of the test, you will be given the option to fill out
-				information about how you describe yourself politcally. There will be 3
-				options. The first will take you to a small survey and will record your
-				answers to that along with the results. The second will just record your
-				results. The third will not record anything. All information recorded is
-				totally anonymous, and is being used for a research project by the
-				creator of <a href="https://altvalues.github.io/">AltValues</a>.
-			</p>
-			<h2>Legal notice</h2>
-			<p>
-				The 8values and 9Axes project licenses grant the rights to &quot;modify,
-				merge, publish, distribute&quot; the software as long as &quot;The above
-				copyright notice and this permission notice shall be included in all
-				copies or substantial portions of the Software.&quot; The included
-				notice can be found <a href="LICENSE.txt">here</a>. This project is
-				released under the same license.
+				In addition, NextCompass uses a modernized development framework, and
+				also fully works with JavaScript disabled!
 			</p>
 		</>
 	);
